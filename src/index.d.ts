3e4b00503ccbf512987bedc6e69030d254220979
Taehyeungkim <kth1837@snu.ac.kr>
2024-09-10 04:37:23 +0900

    declared mp3, wav file module type

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+// declare module "*.mp3" {
+//   const value: string;
+//   export = value;
+// }
+declare module "*.mp3";
+declare module "*.wav";
+
+// declare module "*.wav" {
+//   const value: string;
+//   export = value;
+// }
